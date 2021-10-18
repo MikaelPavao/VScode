327de744ef6ac0c983b7025e256c87369dbb4ccc
MikaelPavao <88803188+MikaelPavao@users.noreply.github.com>
2021-10-18 11:17:10 -0300

    []

---
+++ aulaJavascript/script/main.js
@@ -0,0 +1,32 @@
+/*const minhaImagem = document.querySelector('img')
+minhaImagem.onclick = function(){
+
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'imagens/firefox-icon.png') {
+      minhaImagem.setAttribute ('src','imagens/avatar.png');
+      minhaImagem.setAttribute('alt','Imagem avatar')
+    } else {
+      minhaImagem.setAttribute ('src','imagens/firefox-icon.png');
+    }
+
+}
+*/
+/*let meuBotao = document.querySelector('#botao');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeDoUsuario(){
+    let nome = prompt('Por favor, digite seu nome: ');
+    localStorage.getItem('nome', nome);
+    meuCabecalho.textContent = 'Mozila é legal ' + nome;
+}*/
+/*
+if(!localStorage.getItem('nome')) {
+    defineNomeDoUsuario();
+  } else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent =  'Mozila firefox é legal', nome
+  }
+  //meuBotao.onclick = function(){defineNomeDoUsuario()
+}
+*/
+
